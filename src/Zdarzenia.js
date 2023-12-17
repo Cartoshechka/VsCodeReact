27b6a8cc331819830a27a92ae83313871df4d7f5
Cartoshechka <skalatushkin@gmail.com>
2023-12-17 12:51:51 +0100

    Updt 1

--- src/Zdarzenia.js
+++ src/Zdarzenia.js
@@ -1,29 +1,32 @@
 import { useState } from "react";
 
 
-function Przycisk(){
+function Przycisk({counterGeneral, setCounterGeneral}){
 
     const [counter, setCounter] = useState(0);
 
     function obslugaKlikniencja(){
-        alert("klikniento mnie!");
+
+
+        //alert("klikniento mnie!");
         setCounter(counter + 1)
+        setCounterGeneral(counterGeneral + 1)
     }
 
     return(
     <>
-        <button onClick={obslugaKlikniencja}>Kliknij Mnie [{counter} razy kliknieto]</button>
+        <button onClick={obslugaKlikniencja}>Kliknij Mnie [{counter} razy kliknieto, {counterGeneral} ogolnie]</button>
     </> 
     );
 }
 
 function Zdarzenia(){
-
+    const [counterGeneral, setCounterGeneral] = useState(0);
     return(
         <div>
-            <Przycisk />
+            <Przycisk counterGeneral={counterGeneral} setCounterGeneral={setCounterGeneral} />
             <br />
-            <Przycisk />
+            <Przycisk counterGeneral={counterGeneral} setCounterGeneral={setCounterGeneral}/>
         </div>
     );
 
